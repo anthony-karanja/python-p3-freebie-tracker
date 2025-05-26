69c05f7b8ed1f474c4e7c4d1ac6e84c3387d007a
Anthony <wangonguanthony@gmail.com>
2025-05-27 00:07:08 +0300

    created the seed and the model file to run

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, declarative_base
+
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,7 +16,11 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', back_populates = 'company')
+
     def __repr__(self):
+        from lib.models import Base
+        target_metadata = Base.metadata
         return f'<Company {self.name}>'
 
 class Dev(Base):
@@ -25,5 +29,23 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', back_populates = 'dev')
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+
+class Freebie(Base):
+    __tablename__ = 'freebies'  
+
+    id = Column(Integer(), primary_key=True)
+    item_name =Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    company = relationship('Company', back_populates = 'freebies')
+    dev = relationship('Dev', back_populates= 'freebies')
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name} ${self.value}>'
